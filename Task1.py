401a87d3fe1a3626bcaa9014127571548cf66e71
Philipp Maier <phma41@web.de>
2021-04-08 22:15:54 +0200

    finished Task1

--- Task1.py
+++ Task1.py
@@ -1,3 +1,19 @@
+def check_numbers(phone_list,single_nums):
+    
+    #check first column
+    for i in range(len(phone_list)):
+        if phone_list[i][0] not in single_nums:
+            single_nums.append(texts[i][0])
+            
+    #check second column
+    for i in range(len(phone_list)):
+        if phone_list[i][1] not in single_nums:
+            single_nums.append(texts[i][1])
+    
+    return single_nums
+            
+    
+
 """
 Read file into texts and calls.
 It's ok if you don't understand how to read files.
@@ -18,3 +34,18 @@ How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+#numbers in texts (9072 lines):
+#1)check sending numbers , 2) check receiving numbers
+
+#numbers in calls:
+#1)check calling numbers , 2) check answering numbers 
+
+# list every number that is new in list
+single_nums =[]
+
+single_nums = check_numbers(texts,single_nums)
+single_nums = check_numbers(calls,single_nums)
+
+        
+print("There are {} different telephone numbers in the records."
+      .format(len(single_nums)))
